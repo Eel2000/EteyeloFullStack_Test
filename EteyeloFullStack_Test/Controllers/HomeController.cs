dbd1f4db8b21397ca055690c32d36979a55d25b7
Eel2000 <eliezerbwana@gmaill.com>
2020-08-05 06:03:10 +0200

    last update for using services in controller

--- EteyeloFullStack_Test/Controllers/HomeController.cs
+++ EteyeloFullStack_Test/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EteyeloFullStack_Test.Models;
+using EteyeloFullStack_Test.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
