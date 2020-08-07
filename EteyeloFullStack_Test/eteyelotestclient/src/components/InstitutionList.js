89ffd322faab0b1e05bae93c11c7111106269d25
Eel2000 <eliezerbwana@gmaill.com>
2020-08-07 14:51:04 +0200

    updating design for institionList page

--- EteyeloFullStack_Test/eteyelotestclient/src/components/InstitutionList.js
+++ EteyeloFullStack_Test/eteyelotestclient/src/components/InstitutionList.js
@@ -1,5 +1,7 @@
 ﻿import React, { Component } from 'react';
 import { Navigation } from './Navigation';
+import { InputGroup, FormControl, Button, Table } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 
 export class InstitutionList extends Component {
@@ -7,8 +9,44 @@ export class InstitutionList extends Component {
         return (
             <React.Fragment>
                 <Navigation />
-                <div className="container-fluid">
-                    <h1>list des institutions</h1>
+               
+                <div className="container">
+                    <div className="container">
+                        <h1 className="display-3 text-warning p-5">Institutions/etablissement/Ecoles disponible</h1>
+
+
+                        <InputGroup className="mb-1">
+                            <FormControl
+                                placeholder="Eleve rechercher"
+                                aria-label="Recipient's username"
+                                aria-describedby="basic-addon2"
+                            />
+                            <InputGroup.Append className="col-8 ">
+                                <Button variant="success" type="submit">
+                                    Recherche
+                        </Button>
+                            </InputGroup.Append>
+                        </InputGroup>
+
+                        
+                        <div className="container">
+                            <Table striped bordered hover variant="secondary">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Intitution/ecole</th>
+                                        <th>Email</th>
+                                        <th>Telephone</th>
+                                        <th>----</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </Table>
+                        </div>
+
+                        <NavLink className="nav-link text-left" to="/Commencer">Nouveau</NavLink>
+                    </div>
                 </div>
             </React.Fragment>
             );
